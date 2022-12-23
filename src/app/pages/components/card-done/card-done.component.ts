ea5b6687a76524d767ce52f69708e2c799f582a3
Rahmad Nur Azim <rahmad.azim@aux.dkatalis.com>
2022-12-23 16:23:31 +0700

    add trello done

--- src/app/pages/components/card-done/card-done.component.ts
+++ src/app/pages/components/card-done/card-done.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DONE, IZimlo, PLAN, TODO } from 'src/app/interfaces/i-zimlo';
+import { ZimloService } from 'src/app/services/zimlo.service';
 
 @Component({
   selector: 'app-card-done',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-done.component.css']
 })
 export class CardDoneComponent {
+  tasks: IZimlo[] =[];
+  statusTodo: string = TODO;
+  statusPlan: string = PLAN;
 
+  constructor(private zimloService: ZimloService) { }
+
+  ngOnInit(): void {
+      this.onAll();
+  }
+
+  onAll(): void{
+    this.zimloService.all(DONE).subscribe(
+      (data: any) => {
+        this.tasks = data.map((e: any) => {
+          return {
+            id: e.payload.doc.id,
+            title: e.payload.doc.data()['title'],
+            description: e.payload.doc.data()['description'],
+            status: e.payload.doc.data()['status'],
+          }as IZimlo;
+        })
+      }
+    )
+  }
+
+  onUpdate(id:string, task: IZimlo): void {
+    this.zimloService.update(id, task);
+  }
 }
--- src/app/services/zimlo.service.ts
+++ src/app/services/zimlo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { regExpEscape } from '@ng-bootstrap/ng-bootstrap/util/util';
 import { IZimlo, PLAN } from '../interfaces/i-zimlo';
 
 @Injectable({
@@ -7,7 +8,7 @@ import { IZimlo, PLAN } from '../interfaces/i-zimlo';
 })
 export class ZimloService{
   public isShow: boolean = false;
-  collection: string = "tasks";
+  collection: string = "Tasks";
 
   constructor(private firestore: AngularFirestore) { }
 
@@ -18,5 +19,15 @@ export class ZimloService{
         status: PLAN
       } as IZimlo;
       this.firestore.collection(this.collection).add(payload);
+  }
+
+  all(status: string) {
+    return this.firestore.collection(
+      this.collection,
+      ref => ref.where('status', '==', status)).snapshotChanges();
+  }
+
+  update(id: string, task: IZimlo) {
+    return this.firestore.doc(`${this.collection}/` + id).update(task);
   }
 }
